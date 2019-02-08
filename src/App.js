f8a46741d3da163818407074157611fc85a81c84
kristiwalker <kristiwalker22@gmail.com>
2019-02-08 16:59:36 -0500

    adds initial state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, this is a React app</h1>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is a React app'));
-  }
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stepanie', age: 26 }
+        ]
+    };
+
+    render() {
+        return (<div className="App">
+            <h1>Hi, I'm a React app</h1>
+            <p>This is really working!</p>
+            <button>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div>);
+
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is a React app'));
+    }
 }
 
 export default App;
