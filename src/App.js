98a32a01489d911a6766b80db2ffdaa4b7f9b3cb
kristiwalker <kristiwalker22@gmail.com>
2019-02-20 16:26:21 -0500

    making conditional style arrays

--- src/App.js
+++ src/App.js
@@ -117,9 +117,19 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
+        const classes = [];
+
+        if (this.state.persons.length <= 2) {
+            classes.push('red');
+        }
+
+        if (this.state.persons.length <= 1) {
+            classes.push('bold');
+        }
+
             return (<div className="App">
                 <h1>Hi, I'm a React app</h1>
-                <p>This is really working!</p>
+                <p className={classes.join(' ')}>This is really working!</p>
 
                 {/* <button style={style} onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button> */}
 
